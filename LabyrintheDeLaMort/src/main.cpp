96fae612090be60f573017e19565766a15963fcf
Jonathan Klukach Fernandez <jonathan.klukach.fernandez@gmail.com>
2021-10-21 16:05:27 +0200

    Updated settings

--- LabyrintheDeLaMort/src/main.cpp
+++ LabyrintheDeLaMort/src/main.cpp
@@ -5,12 +5,16 @@
 #include <iostream>
 #include <chrono>
 #include <random>
+#include "Potion.h"
 
 using std::cout;
 using std::endl;
 
 int main()
 {
+	Potion p;
+	p.drink();
+
 	// obtain a seed from the system clock
 	const uint32_t seed = static_cast<uint32_t>(
 		std::chrono::high_resolution_clock::now().time_since_epoch().count());
