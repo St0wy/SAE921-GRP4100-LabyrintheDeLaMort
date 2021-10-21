8bf75a8402bd697c305c9955450b3d74d261aad5
Stowy <stowy@protonmail.ch>
2021-10-21 16:28:20 +0200

    updated comments in main

--- LabyrintheDeLaMort/src/main.cpp
+++ LabyrintheDeLaMort/src/main.cpp
@@ -12,13 +12,13 @@ using std::endl;
 
 int main()
 {
-
-	// obtain a seed from the system clock
-	const uint32_t seed = static_cast<uint32_t>(
+	// Get the current time
+	const uint32_t current_time = static_cast<uint32_t>(
 		std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
 	// Create the rnd generator
-	std::default_random_engine generator(seed);
+	// Takes the current time as a seed
+	std::default_random_engine generator(current_time);
 
 	//Entity one = Entity(generator);
 	//Entity two = Entity(generator);
