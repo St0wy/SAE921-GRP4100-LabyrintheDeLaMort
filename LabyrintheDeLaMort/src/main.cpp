657adb331abd29e44696e723731d1d23e479055b
Fabian Huber <stowy@protonmail.ch>
2021-11-17 10:13:10 +0100

    hid the console

--- LabyrintheDeLaMort/src/main.cpp
+++ LabyrintheDeLaMort/src/main.cpp
@@ -2,11 +2,45 @@
 #include "Item.h"
 
 #include <iostream>
-#include <chrono>
-#include <random>
-#include "Potion.h"
+#include <string>
+#include <SFML/Graphics.hpp>
+
+constexpr int WINDOW_WIDTH = 900;
+constexpr int WINDOW_HEIGHT = 600;
+constexpr int FRAMERATE = 60;
+constexpr char WINDOW_NAME[] = "Labyrinthe de la mort";
 
 int main()
 {
-	std::cin.get();
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME);
+
+    // Basic Setup of the window
+    // Vertical sync, framerate
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(FRAMERATE);
+
+    while (window.isOpen())
+    {
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis
+        // la précédente itération
+        sf::Event event{};
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                // évènement "fermeture demandée" : on ferme la fenêtre
+            case sf::Event::Closed:
+                window.close();
+                break;
+            default:
+                break;
+            }
+        }
+
+        // Graphical Region
+        window.clear(sf::Color::White);
+
+        window.display();
+    }
 }
