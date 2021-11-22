1c81993561643fb3297a0158a1c3ca119ab61d66
Fabian Huber <stowy@protonmail.ch>
2021-11-22 10:42:33 +0100

    commented on some functions

--- LabyrintheDeLaMort/include/Entity.h
+++ LabyrintheDeLaMort/include/Entity.h
@@ -21,9 +21,23 @@ public:
 	Entity& operator=(const Entity& other) = default;
 	Entity& operator=(Entity&& other) noexcept = default;
 
+	/**
+	 * \brief Sets the texture of the entity.
+	 * \param texture Texture to apply to the entity.
+	 * \param texture_rect Rectangle that will be used as the texture.
+	 */
 	void set_texture(const sf::Texture& texture, const sf::IntRect& texture_rect);
+
+	/**
+	 * \brief Gets the sprite of the Entity.
+	 * \return The sprite of the Entity.
+	 */
 	sf::Sprite& get_sprite();
 
+	/**
+	 * \brief Updates the entity. Should be implemented by child classes.
+	 * \param delta_time Time elapsed since the last frame.
+	 */
 	virtual void update(sf::Time delta_time) = 0;
 
 	void on_draw(sf::RenderTarget& target, sf::RenderStates states) const override;
--- LabyrintheDeLaMort/include/SceneNode.h
+++ LabyrintheDeLaMort/include/SceneNode.h
@@ -15,7 +15,17 @@ public:
 	SceneNode& operator=(const SceneNode& other) = default;
 	SceneNode& operator=(SceneNode&& other) noexcept = default;
 
+	/**
+	 * \brief Draws the SceneNode and all of its children.
+	 * \param target Render target to render on.
+	 * \param states Transformations of the parent objects.
+	 */
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 private:
+	/**
+	 * \brief Renders the node on the target.
+	 * \param target Render target to render on.
+	 * \param states Transformations of the parent objects.
+	 */
 	virtual void on_draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
 };
