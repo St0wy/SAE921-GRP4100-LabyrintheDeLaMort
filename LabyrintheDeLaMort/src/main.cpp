2a2dde3cd1352a08719b1c3c840ae2494168a739
Fabian Huber <stowy@protonmail.ch>
2021-11-22 10:24:28 +0100

    Added comments in main.cpp

--- LabyrintheDeLaMort/src/main.cpp
+++ LabyrintheDeLaMort/src/main.cpp
@@ -6,24 +6,24 @@
 
 int main()
 {
+	// Create random engine
 	const auto seed = static_cast<uint32_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 	std::default_random_engine gen(seed);
 
+	// Create render window
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME, sf::Style::Close);
-
-	// Basic Setup of the window
-	// Vertical sync, framerate
 	window.setVerticalSyncEnabled(true);
 	window.setFramerateLimit(FRAMERATE);
 
+	// Create view (camera) of the scene and set it to the window
 	sf::View view(sf::Vector2f(0, 0), sf::Vector2f(CAM_SIZE_X, CAM_SIZE_Y));
-
 	window.setView(view);
 
+	// Load the spritesheet
 	sf::Texture char_spritesheet;
 	char_spritesheet.loadFromFile("./data/sprites/char_spritesheet.png");
-	char_spritesheet.setSmooth(false);
 
+	// Create the vector of entities
 	std::vector<std::unique_ptr<Entity>> entities;
 	entities.emplace_back(std::make_unique<Hero>(gen));
 	entities[0]->set_texture(char_spritesheet, sf::IntRect(sf::Vector2i(0, 80), SPRITE_SIZE));
@@ -31,7 +31,7 @@ int main()
 	sf::Clock clock;
 	while (window.isOpen())
 	{
-		auto delta_time = clock.restart();
+		sf::Time delta_time = clock.restart();
 		sf::Event event{};
 
 		while (window.pollEvent(event))
@@ -46,19 +46,20 @@ int main()
 			}
 		}
 
+		// Update all the entities
 		for (auto& entity : entities)
 		{
 			entity->update(delta_time);
 		}
 
-		// Graphical Region
+		// Rendering
 		window.clear(sf::Color::Black);
 
+		// Render all the entities
 		for (auto& entity : entities)
 		{
 			window.draw(*entity);
 		}
-
 		window.display();
 	}
 }
