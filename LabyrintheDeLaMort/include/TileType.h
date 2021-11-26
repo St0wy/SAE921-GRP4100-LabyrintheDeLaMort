9bafd0e3053a9d001d9a2955a3fa373a31be7af6
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2021-11-26 15:51:35 +0100

    coordonates update

--- LabyrintheDeLaMort/include/TileType.h
+++ LabyrintheDeLaMort/include/TileType.h
@@ -3,6 +3,57 @@
 enum class TileType
 {
 	Empty,
+
+	//ligne1
+	Gold,
+	Barrel,
+	Shelf,
+	GreenBanner,
+	RedBanner,
+	Key,
+	GreenPotion,
+	RedPotion,
+	YellowPotion,
+
+	//ligne2
+	GreenTableLeft,
+	GreenTableRight,
+	Skeleton,
+	RedTable,
+
+	//ligne4
+	HorizontalLines,
+	Stairs,
+
+	//ligne5
+	BottomLeftWall,
+	BottomRightWall,
+	Wall1,
+	Wall2,
+	Wall3,
+
+	//ligne6
+	LeftWall,
+	RightWall,
+	Wall4,
+	Wall5,
+	Wall6,
+
+	//ligne7
+	BottomCornerLeftWall,
+	BottomWall,
+	LeftWall2,
+	RightWall2,
+	UpperWall2,
+	UpperWall3,
+	UpperWall4,
+
+	//ligne8
+	LeftAndBottomWall,
+	BottomWall2,
+	BottomAndRightWall,
+
+	//other
 	DirtOne,
 	DirtTwo,
 	BSquare,
--- LabyrintheDeLaMort/src/Tile.cpp
+++ LabyrintheDeLaMort/src/Tile.cpp
@@ -63,5 +63,75 @@ sf::Vector2i Tile::get_tile_pos() const
 	case TileType::Empty:
 	default:  // NOLINT(clang-diagnostic-covered-switch-default)
 		return { 0, 4 };
+	case TileType::Gold:
+		return{0,0};
+	case TileType::Barrel:
+		return{1,0};
+	case TileType::Shelf:
+		return{2,0};
+	case TileType::GreenBanner:
+		return{3,0};
+	case TileType::RedBanner:
+		return{4,0};
+	case TileType::Key:
+		return{5,0};
+	case TileType::GreenPotion:
+		return{6,0};
+	case TileType::RedPotion:
+		return{7,0};
+	case TileType::YellowPotion:
+		return{8,0};
+	case TileType::GreenTableLeft:
+		return {0,1};
+	case TileType::GreenTableRight:
+		return{1,1};
+	case TileType::Skeleton:
+		return{4,1};
+	case TileType::RedTable:
+		return{5,1};
+	case TileType::HorizontalLines:
+		return{0,3};
+	case TileType::Stairs:
+		return{1,3};
+	case TileType::BottomLeftWall:
+		return{3,4};
+	case TileType::BottomRightWall:
+		return{4,4};
+	case TileType::Wall1:
+		return{6,4};
+	case TileType::Wall2:
+		return{7,4};
+	case TileType::Wall3:
+		return{8,4};
+	case TileType::LeftWall:
+		return{3,5};
+	case TileType::RightWall:
+		return{4,5};
+	case TileType::Wall4:
+		return{6,5};
+	case TileType::Wall5:
+		return{7,5};
+	case TileType::Wall6:
+		return{8,5};
+	case TileType::BottomCornerLeftWall:
+		return{1,6};
+	case TileType::BottomWall:
+		return{2,6};
+	case TileType::LeftWall2:
+		return{3,6};
+	case TileType::RightWall2:
+		return{4,6};
+	case TileType::UpperWall2:
+		return{5,6};
+	case TileType::UpperWall3:
+		return{7,6};
+	case TileType::UpperWall4:
+		return{ 8,6 };
+	case TileType::LeftAndBottomWall:
+		return{1,7};
+	case TileType::BottomWall2:
+		return{2,7};
+	case TileType::BottomAndRightWall:
+		return{3,7};
 	}
 }
